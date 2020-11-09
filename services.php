1e0f1d687174aac8f3c488ad8c361b3196f3c0e5
tumwine <tumwineivan20@gmail.com>
2020-11-09 14:23:15 +0300

    Update services.php

--- services.php
+++ services.php
@@ -15,6 +15,9 @@
     <meta charset="UT-F">
     <meta name="viewport" content="width=device-width, intial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <!--font awesome-->
+    <link rel="stylesheet" href="assets/css/fonts.css">
 
      <!--font awesome-->
     <link rel="stylesheet" href="assets/css/all.css">
