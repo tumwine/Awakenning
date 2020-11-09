4aa2ae61e6851ea90e70eae2c6e7a4c302071f1c
tumwine <tumwineivan20@gmail.com>
2020-11-09 12:56:52 +0300

    Update connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name ="blog";
+$host = "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "hzwhr1kkj9flbtj3";
+$pass = "b639582ngy93stq9";
+$db_name ="uf68dny9zi9edgh9";
 
 $conn = new MySQLi($host,$user,$pass,$db_name);
 
