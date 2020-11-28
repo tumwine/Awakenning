c6e44111f6bfbd6715d39f1fd78abf1500f028eb
tumwine <tumwineivan20@gmail.com>
2020-11-28 11:23:26 -0500

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ ini_set("sendmail_from", "tumwineivan20@gmail.com");
 
 $message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
 
-$headers = "From: YOURMAIL@gmail.com";
+$headers = "From: tumwineivan20@gmail.com";
 
 mail("tumwineivan20@gmail.com", "Testing", $message, $headers);
 echo "Check your email now....&lt;BR/>";
