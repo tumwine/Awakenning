8f03999428e98e0bd89920e5fca40aeb06d18348
tumwine <tumwineivan20@gmail.com>
2020-11-28 11:20:12 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
 ini_set("SMTP", "aspmx.l.google.com");
-ini_set("sendmail_from", "YOURMAIL@gmail.com");
+ini_set("sendmail_from", "tumwineivan20@gmail.com");
 
 $message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
 
 $headers = "From: YOURMAIL@gmail.com";
 
-mail("Sending@provider.com", "Testing", $message, $headers);
+mail("tumwineivan20@gmail.com", "Testing", $message, $headers);
 echo "Check your email now....&lt;BR/>";
 ?>
